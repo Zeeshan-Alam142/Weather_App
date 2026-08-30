30c3cabcd7b8377fa40fbe6f1e35d55935e63d87
Zeeshan <zeeshanalam1261@gmail.com>
2026-08-30 17:43:23 +0530

    Refactor weather check and fix image paths
    Refactored weather fetching logic and updated image paths.

--- newproject.js
+++ newproject.js
@@ -1,53 +1,48 @@
+const apiKey="4b982d3aa7c3bbf9f6c6c0f3eeb931f7";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-        const apiKey="4b982d3aa7c3bbf9f6c6c0f3eeb931f7";
-        const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weather_icon = document.querySelector(".weather_icon");
 
-        const searchBox = document.querySelector(".search input");
-        const searchBtn = document.querySelector(".search button");
-        const weather_icon = document.querySelector(".weather_icon")
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-        async function checkWeather(city){
-            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
 
-            if(response.status == 404){
-                document.querySelector(".error").style.display = "block";
-                document.querySelector(".weather").style.display = "none";
-            }else{
-                 const data = await response.json();
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-            
-
-            document.querySelector(".city").innerHTML = data.name;
-            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-            document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-            if(data.weather[0].main == "Clouds"){
-                weather_icon.src = "images/clouds.png";
-            }
-            else if(data.weather[0].main == "Clear"){
-                weather_icon.src = "images/clear.png";
-            }
-            else if(data.weather[0].main == "Rain"){
-                weather_icon.src = "images/rain.png";
-            }
-            else if(data.weather[0].main == "Drizzle"){
-                weather_icon.src = "images/drizzle.png";
-            }
-            else if(data.weather[0].main == "Mist"){
-                weather_icon.src = "images/mist.png";
-            }
-            document.querySelector(".error").style.display = "none";
-            document.querySelector(".weather").style.display = "block";
-
-            }
-
-           
-
+        // Fixed image paths: removed "images/" prefix to match your root folder
+        if(data.weather[0].main == "Clouds"){
+            weather_icon.src = "clouds.png";
         }
-        searchBtn.addEventListener("click", ()=>{
-            checkWeather(searchBox.value);
-        })
+        else if(data.weather[0].main == "Clear"){
+            weather_icon.src = "clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weather_icon.src = "rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weather_icon.src = "drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weather_icon.src = "mist.png";
+        }
+        
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
+}
 
-      
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
     
